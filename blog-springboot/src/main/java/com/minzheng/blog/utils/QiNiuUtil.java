d1604472fd7b4b7608e9f6963e7c63a269135c44
xxmy <1471441495@qq.com>
2021-07-12 16:14:52 +0800

    feat: 优化配置文件

--- blog-springboot/src/main/java/com/minzheng/blog/utils/QiNiuUtil.java
+++ blog-springboot/src/main/java/com/minzheng/blog/utils/QiNiuUtil.java
@@ -1,30 +1,21 @@
 package com.minzheng.blog.utils;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.qiniu.common.QiniuException;
-import com.qiniu.common.Zone;
 import com.qiniu.http.Response;
-import com.qiniu.storage.BucketManager;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
-import com.qiniu.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 /**
  * @Author: lyh
